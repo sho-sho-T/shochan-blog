602d56b5654ca4c56126486c525d6d976185607a
ShotoMatsubara <shoto0628matsubara@gmail.com>
2025-05-03 23:58:32 +0000

    feat: メタデータにOpen Graph情報とcanonical URLを追加

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -8,7 +8,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shochan.dev",
-  description: "日々の学習、学びなどを雑に投稿します",
+  description: "雑に学習、学びなどを投稿します",
+  openGraph: {
+    title: "Shochan.dev",
+    description: "雑に学習、学びなどを投稿します",
+    type: "website",
+    url: "https://shochan-blog.vercel.app",
+  },
+  alternates: {
+    canonical: "https://shochan-blog.vercel.app",
+  },
 };
 
 export default function RootLayout({
